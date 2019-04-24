f8dd9793d1409613c02da8582404a582f7463b0e
Yanina Ortiz <yanhaina@hotmail.com>
2019-04-24 17:49:49 -0400

    Ya esta correcto

--- persona1.py
+++ persona1.py
@@ -4,12 +4,9 @@ class persona:
    
     def __init__(self, un_nombre, edad):
         self.nombre_persona= un_nombre
-        self.edad= edad1
-        print("Hola, soy un persona y me llamo", self.nombre_persona, "mi edad es", self.edad)
+        self.edad= edad
+        print("Hola, soy un persona y me llamo", self.nombre_persona, "mi edad es", edad)
 Luli= persona("Luli", 16)
-
-
-
 
 
 

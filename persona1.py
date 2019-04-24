ab349c66a19ce889491d63d3d2b64c3001519bd2
Yanina Ortiz <yanhaina@hotmail.com>
2019-04-24 16:57:31 -0400

    Al fin se guarda

--- persona1.py
+++ persona1.py
@@ -1,14 +1,22 @@
-
 class persona: 
     nombre_persona= None
-    def __init__(self, un_nombre):
+    edad= None
+   
+    def __init__(self, un_nombre, edad):
         self.nombre_persona= un_nombre
-        print("Hola, soy un persona y me llamo", self.nombre_persona)
+        self.edad= edad1
+        print("Hola, soy un persona y me llamo", self.nombre_persona, "mi edad es", self.edad)
+Luli= persona("Luli", 16)
 
-    def set_nombre(self, nombre):
-        self.nombre_persona= nombre
-    
-Luli= persona("Luli")
+
+
+
+
+
+
+
+
+
 
 
 

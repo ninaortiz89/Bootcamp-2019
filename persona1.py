a7a7d673e8e1305d4c2253d33cb4e0f4aae83ddb
Yanina Ortiz <yanhaina@hotmail.com>
2019-04-24 16:34:26 -0400

    Se agrega nuevo archivo

---
+++ persona1.py
@@ -0,0 +1,16 @@
+
+class persona: 
+    nombre_persona= None
+    def __init__(self, un_nombre):
+        self.nombre_persona= un_nombre
+        print("Hola, soy un persona y me llamo", self.nombre_persona)
+
+    def set_nombre(self, nombre):
+        self.nombre_persona= nombre
+    
+Luli= persona("Luli")
+
+
+
+    
+ 

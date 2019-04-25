cead23c26bf80112f090180d1e668808d1b75497
Yanina Ortiz <yanhaina@hotmail.com>
2019-04-25 15:42:47 -0400

    Diccionario1

---
+++ Day 4.py
@@ -0,0 +1,74 @@
+"""diccionario= {}
+
+nombre_ de_ diccionario= {"nombre_clave":"valor"}
+#Lista
+personallist= ["Yanina", 29, "Lic."]
+#Diccionario
+
+dic_persona= {"nombre": "Yanina", "edad": 29}
+print(dic_persona)
+print(dic_persona["edad"])
+dic_persona["edad"]= "Adivina mi edad"
+print(dic_persona["edad"])
+print(personallist[1])
+
+dic_persona["profesion"]= "Lic"
+print(dic_persona)
+print (dic_persona["profesion"])
+print(dic_persona.get("estatura"))
+
+
+dic_persona2=   {
+                "nombre": "Yani"
+                "edad": 29
+                "color de cabello": "castanho"
+                }  
+
+print(dic_ persona2)"""
+
+
+#Crear un diccionario persona con claves nombre, edad, 
+# estatura e imprimir cada uno de los valores de las claves en un print diferente
+#Luego cambiar edad a otro valor e imprimir de nuevo 
+
+# Luego agregar un par clave/valor "Hobbie" que contenga una ***lista*** 
+# de hobbies e imprimir todo el diccionario
+
+
+dictionary_list= {"nombre": "Yanina", "edad": 29, "estatura": "1.57 cm"}
+print(dictionary_list)
+print(dictionary_list ["nombre"])
+print(dictionary_list ["edad"])
+print(dictionary_list ["estatura"])
+edad1= input ("Decime otra edad")
+print(edad1)
+hobbie= ["maraton de series",  "escalar", "viajar"]
+print(hobbie)
+dictionary_list["hobbie"]= hobbie 
+print(dictionary_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
